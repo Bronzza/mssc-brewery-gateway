6632df3d277c4d4190809f329e83657bbf497351
Serhii Repinetskyi <repinetskiy@gmail.com>
2024-06-18 11:56:47 +0700

    Initial commit

---
+++ src/main/java/com/gateway/brewary_gateway/BrewaryGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.gateway.brewary_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewaryGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewaryGatewayApplication.class, args);
+	}
+
+}
