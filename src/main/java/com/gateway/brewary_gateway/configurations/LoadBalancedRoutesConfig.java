065e44b546813c0c5c2ff6004234f36275e4d281
Serhii Repinetskyi <repinetskiy@gmail.com>
2024-06-21 17:16:28 +0700

    Working conguration for routing. Two profiles, one with Eureka another without (using ports).

--- src/main/java/com/gateway/brewary_gateway/configurations/LoadBalancedRoutesConfig.java
+++ src/main/java/com/gateway/brewary_gateway/configurations/LoadBalancedRoutesConfig.java
@@ -1,31 +1,37 @@
 package com.gateway.brewary_gateway.configurations;
 
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
-@Profile("!local-discovery")
+@Profile("local-discovery")
 @Configuration
-public class LocalHostRouteConfig {
-
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class LoadBalancedRoutesConfig {
     @Bean
-    public RouteLocator localHostLocator(RouteLocatorBuilder builder) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
         return builder
                 .routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUps/*", "/api/v1/beerUps*")
-                        .uri("http://localhost:8080")
+                        .uri("lb://beer-service")
                         .id("beer-service"))
                 .route(r -> r.path("/api/v1/customers*", "/api/v1/customers/*", "/api/v1/customers/**")
-                        .uri("http://localhost:8081")
+                        .uri("lb://order-service")
                         .id("order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory","/api/v1/beer*/inventory")
-                        .uri("http://localhost:8082")
+                        .filters(f -> f.circuitBreaker(c -> c.setName("inventoryCB")
+                                .setFallbackUri("forward:/inventory-failover")
+                                .setRouteId("inv-failover")
+                        ))
+                        .uri("lb://inventory-service")
                         .id("inventory-service"))
-                .route(r -> r.path("/api/v1/inventory/*","/api/v1/inventory*")
-                        .uri("http://localhost:8083")
+                .route(r -> r.path("/inventory-failover")
+                        .uri("lb://inventory-failover")
                         .id("inventory-failover"))
                 .build();
     }
